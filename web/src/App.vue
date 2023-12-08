32c8200ba539ef99dbad6ce16988df08ed819d8a
fifpoet <fifpoet@qq.com>
2023-12-08 21:42:19 +0800

    feat_初始化底图

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import Map from "./components/Map.vue";
 </script>
 
 <template>
@@ -11,7 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Map></Map>
 </template>
 
 <style scoped>
---
+++ web/src/components/Map.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="map-container" id="map-container"></div>
+</template>
+
+<script>
+export default {
+  name: "mapView",
+  components: {},
+  data() {
+    return {
+      map: null,
+      OSMUrl: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    };
+  },
+  mounted() {
+    this.map = this.$utils.map.createMap("map-container");
+    // 设施地图视图 中心位置
+    this.map.setView([30.35, 114.18], 13);
+
+    // 加载 open street map和mapbox 图层服务
+    this.$utils.map.createTileLayer(this.map, this.OSMUrl, {maxZoom: 19});
+  }
+};
+</script>
+<style scoped>
+.map-container {
+  position: absolute;
+  left: 0;
+  top: 100px;
+  width: 100%;
+  height: 100%;
+}
+</style>
